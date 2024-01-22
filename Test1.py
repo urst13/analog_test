079193e90024baabd23c7a45a1da88adc1838a76
Hendrik <hendrikhusmann@gmail.com>
2024-01-22 21:32:43 +0100

    Grundlegenden Code hinzugefügt

--- Test1.py
+++ Test1.py
@@ -1,1 +1,21 @@
 # Kontinuierliches Auslesen des A0-Eingangs, print in Konsole
+
+import board
+import time
+import busio
+import adafruit_ads1x15.ads1115 as ADS
+from adafruit_ads1x15.analog_in import AnalogIn
+ 
+# Initialize the I2C interface
+i2c = busio.I2C(board.SCL, board.SDA)
+ 
+# Create an ADS1115 object
+ads = ADS.ADS1115(i2c)
+ 
+# Define the analog input channel
+channel = AnalogIn(ads, ADS.P0)
+ 
+# Loop to read the analog input continuously
+while True:
+    print("Analog Value: ", channel.value, "Voltage: ", channel.voltage)
+    time.sleep(0.2)
